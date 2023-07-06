433186d656fc235fac3d26455a818adc349975cf
Noman Mahmud <molla.noman23@gmail.com>
2023-07-06 19:18:51 +0600

    feat: create loading.js file

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="bg-slate-900 text-slate-50 px-6">
       <Header />
