a3917fe6fb52963c5f13b02ab253425d188f002a
Mojammel Noman <molla.noman23@gmail.com>
2024-07-15 18:50:22 +0600

    add express and redefined routes

--- backend/server.js
+++ backend/server.js
@@ -2,47 +2,28 @@
  * It's the backend of portfolio project that takes a http request from the client and returns appropriate response with the data.
  */
 
-const http = require("http");
+const express = require("express");
 
+const app = express();
+const port = process.env.PORT || 4000;
+
+// import all data
 const about = require("./data/about");
 const projects = require("./data/projects");
 const footer = require("./data/footer");
 
-const server = http.createServer((req, res) => {
-  // CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.url === "/") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-
-    res.end(JSON.stringify(about));
-  } else if (req.url === "/projects") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-
-    res.end(JSON.stringify(projects));
-  } else if (req.url === "/footer") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-
-    res.end(JSON.stringify(footer));
-  } else {
-    res.writeHead(400, {
-      "Content-Type": "text/plain",
-    });
-
-    res.end("Bad Request");
-  }
+app.get("/", (req, res) => {
+  res.json(about);
 });
 
-const PORT = 4000;
+app.get("/projects", (req, res) => {
+  res.json(projects);
+});
 
-server.listen(PORT, "localhost", () => {
-  console.log(`The server is listenning on port ${PORT}`);
+app.get("/footer", (req, res) => {
+  res.json(footer);
+});
+
+app.listen(port, () => {
+  console.log(`The server is listenning on port ${port}`);
 });
