ccfc1b460275c3ed3f7f61af72c6f4bf98b412fe
Mojammel Noman <molla.noman23@gmail.com>
2024-07-15 19:26:34 +0600

    add earnbuddy project info

--- backend/data/projects/index.js
+++ backend/data/projects/index.js
@@ -1,38 +1,22 @@
-const fs = require("fs");
-
-const youtubeImg = fs.readFileSync(`${__dirname}/youtube.png`, {
-  encoding: "base64",
-});
-
-const ecommerceImg = fs.readFileSync(`${__dirname}/ecommerce.png`, {
-  encoding: "base64",
-});
-
-const hoobankImg = fs.readFileSync(`${__dirname}/hoobank.png`, {
-  encoding: "base64",
-});
-
 const projects = [
+  {
+    name: "EarnBuddy",
+    desc: "The EarnBuddy is a Platform where users can complete small tasks and earn rewards.",
+    img: "https://i.ibb.co/jrsYYb4/earn-Buddy.png",
+    liveLink: "https://earnbuddy-dbb3d.firebaseapp.com/",
+    source: {
+      client: "https://github.com/noman023/earnBuddy-client",
+      server: "https://github.com/noman023/earnBuddy-server",
+    },
+  },
   {
     name: "Youtube Clone",
     desc: "This is a youtube clone project. created using rapid api and react js.",
-    img: youtubeImg,
+    img: "",
     liveLink: "https://youtube-clone-noman.netlify.app/",
-    githubLink: "https://github.com/noman023/youtube-clone",
-  },
-  {
-    name: "FoodExpress",
-    desc: "This is an ecommerce project we've created in a team collaboration as our first react project at a course we're in.",
-    img: ecommerceImg,
-    liveLink: "https://foodexpress-0x.vercel.app/",
-    githubLink: "https://github.com/noman023/foodExpress-client",
-  },
-  {
-    name: "Hoobank",
-    desc: "This project created from a figma design to a react project.",
-    img: hoobankImg,
-    liveLink: "https://hoobank-noman.netlify.app/",
-    githubLink: "https://github.com/noman023/figma-to-react",
+    source: {
+      client: "https://github.com/noman023/youtube-clone",
+    },
   },
 ];
 
