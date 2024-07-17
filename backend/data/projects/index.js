1da9fcf3c70824210b20adb7823cadaa3ec597bc
Mojammel Noman <molla.noman23@gmail.com>
2024-07-18 00:59:42 +0600

    add sourcelink

--- backend/data/projects/index.js
+++ backend/data/projects/index.js
@@ -4,19 +4,28 @@ const projects = [
     desc: "The EarnBuddy is a Platform where users can complete small tasks and earn rewards.",
     img: "https://i.ibb.co/jrsYYb4/earn-Buddy.png",
     liveLink: "https://earnbuddy-dbb3d.firebaseapp.com/",
-    source: {
-      client: "https://github.com/noman023/earnBuddy-client",
-      server: "https://github.com/noman023/earnBuddy-server",
-    },
+    sourceLink: [
+      {
+        name: "Clinet",
+        link: "https://github.com/noman023/earnBuddy-client",
+      },
+      {
+        name: "Server",
+        link: "https://github.com/noman023/earnBuddy-server",
+      },
+    ],
   },
   {
     name: "Youtube Clone",
     desc: "This is a youtube clone project. created using rapid api and react js.",
     img: "",
     liveLink: "https://youtube-clone-noman.netlify.app/",
-    source: {
-      client: "https://github.com/noman023/youtube-clone",
-    },
+    sourceLink: [
+      {
+        name: "Client",
+        link: "https://github.com/noman023/youtube-clone",
+      },
+    ],
   },
 ];
 
