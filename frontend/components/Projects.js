e84375787fe75fcbe4b819d8b58e5f1a6eac6c50
Mojammel Noman <molla.noman23@gmail.com>
2024-07-18 00:59:04 +0600

    card redesign

--- frontend/components/Projects.js
+++ frontend/components/Projects.js
@@ -1,34 +1,41 @@
 import axios from "axios";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { dancingScript } from "@/app/layout";
 
 export default async function Projects() {
   const res = await axios.get("http://localhost:4000/projects");
 
   const data = res.data;
+
   return (
     <section className="mt-12 sm:mt-20">
       <div className="container px-5 mx-auto">
         <div>
-          <h1 className="flex items-center justify-center text-2xl sm:text-3xl font-semibold uppercase underline mb-10">
+          <h1
+            className={`text-center text-4xl uppercase mb-10 ${dancingScript.className}`}
+          >
             projects
           </h1>
         </div>
 
-        <div className="flex flex-wrap -m-4">
-          {/* card */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {data &&
             data.map((obj) => (
-              <div className="p-4 md:w-1/3">
-                <div className="h-full flex flex-col justify-between border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                  <div className="p-3">
+              // card
+              <div className="border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden p-3">
+                {/* this div ensures buttons div to stay at bottom */}
+                <div className="flex flex-col h-full space-y-6">
+                  {/* img div */}
+                  <div>
                     <img
-                      className="lg:h-48 md:h-36 w-full"
+                      className="w-full h-48"
                       src={obj.img}
-                      alt="blog"
+                      alt="project image"
                     />
                   </div>
 
-                  <div className="p-3">
+                  {/* project's info div */}
+                  <div className="space-y-1 flex-grow">
                     <div className="flex justify-between items-center">
                       <h1 className="text-xl font-semibold text-white mb-1">
                         {obj.name}
@@ -37,33 +44,28 @@ export default async function Projects() {
                       <a
                         href={obj.liveLink}
                         target="_blank"
-                        className="flex items-center gap-1 hover:text-gray-400"
+                        className="flex items-center gap-1 text-sm hover:text-gray-400 duration-300"
                       >
                         Live <FaArrowRightLong />
                       </a>
                     </div>
 
-                    <p className=" text-base text-slate-400 mb-5">{obj.desc}</p>
+                    <p className="text-base text-slate-400">{obj.desc}</p>
                   </div>
 
-                  <div className="p-3">
-                    {Object.keys(obj).length === 5 ? (
-                      <div className="flex items-center justify-between text-base font-medium">
-                        <a href={obj.githubLink} target="_blank">
-                          <button className="w-[100px] rounded-lg bg-purple-800 hover:bg-purple-900 capitalize p-1 duration-200">
-                            github
-                          </button>
-                        </a>
-                      </div>
-                    ) : (
-                      <div className="flex items-center justify-center text-base ">
-                        <a href={obj.githubLink} target="_blank">
-                          <button className="w-[100px] rounded-lg bg-purple-800 hover:bg-purple-900 capitalize p-1 duration-200 font-medium">
-                            github
-                          </button>
-                        </a>
-                      </div>
-                    )}
+                  {/* buttons div */}
+                  <div className="flex items-center justify-between text-sm">
+                    {obj.sourceLink.map((src, idx) => (
+                      <a href={src.link} target="_blank" key={idx}>
+                        <button
+                          className={`rounded-lg bg-purple-800 hover:bg-purple-900 ${
+                            idx === 1 && "bg-blue-800 hover:bg-blue-900"
+                          } capitalize py-2 px-4 duration-300`}
+                        >
+                          {src.name}
+                        </button>
+                      </a>
+                    ))}
                   </div>
                 </div>
               </div>
