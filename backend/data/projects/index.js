2f82b87449841519e132f1ef38fe83e42f8585b5
Mojammel Noman <molla.noman23@gmail.com>
2024-08-20 19:52:58 +0600

    add 3 more project data

--- backend/data/projects/index.js
+++ backend/data/projects/index.js
@@ -1,7 +1,7 @@
 const projects = [
   {
-    name: "EarnBuddy",
-    desc: "The EarnBuddy is a Platform where users can complete small tasks and earn rewards.",
+    name: "Earn Buddy",
+    desc: "The EarnBuddy is a Platform where users can complete small tasks and earn rewards. Three distinguished user role (admin, task creator, worker) implemented.",
     img: "https://i.ibb.co/jrsYYb4/earn-Buddy.png",
     liveLink: "https://earnbuddy-dbb3d.firebaseapp.com/",
     sourceLink: [
@@ -16,9 +16,53 @@ const projects = [
     ],
   },
   {
+    name: "Help Connect",
+    desc: "A volunteer management application where users can post for volunteer needs and can request for being volunteer for others post etc.",
+    img: "https://i.ibb.co/w62hmRm/help-Connect.png",
+    liveLink: "https://assignment11-c9a59.web.app/",
+    sourceLink: [
+      {
+        name: "Clinet",
+        link: "https://github.com/noman023/helpConnect-client",
+      },
+      {
+        name: "Server",
+        link: "https://github.com/noman023/helpConnect-server",
+      },
+    ],
+  },
+  {
+    name: "Art Gallery",
+    desc: "A plateform to sell users art and crafts item they've created.",
+    img: "https://i.ibb.co/BPKF925/art-Gallery-PNG.png",
+    liveLink: "https://assignment10-4026e.web.app/",
+    sourceLink: [
+      {
+        name: "Clinet",
+        link: "https://github.com/noman023/artGallery-client",
+      },
+      {
+        name: "Server",
+        link: "https://github.com/noman023/artGallery-server",
+      },
+    ],
+  },
+  {
+    name: "Todo Application",
+    desc: "A todo application. created with redux-toolkit.",
+    img: "https://i.ibb.co/c1mqFbh/todo-app.png",
+    liveLink: "https://todo-app-noman.netlify.app/",
+    sourceLink: [
+      {
+        name: "Client",
+        link: "https://github.com/noman023/todo-app",
+      },
+    ],
+  },
+  {
     name: "Youtube Clone",
     desc: "This is a youtube clone project. created using rapid api and react js.",
-    img: "",
+    img: "https://i.ibb.co/BVQFpmS/youtube.png",
     liveLink: "https://youtube-clone-noman.netlify.app/",
     sourceLink: [
       {
