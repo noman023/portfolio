ebe74e06a3668c3498b6b42ce10c7091caa68fef
noman-matteit <noman@matteit.com>
2025-05-18 13:37:12 +0600

    update resume link

--- backend/data/about/index.js
+++ backend/data/about/index.js
@@ -3,7 +3,7 @@ const about = {
   details:
     "A JavaScript Developer with a focus on frontend development using React.js ecosystem. Familiar with backend technologies like Node.js and express.js, as well as databases such as MongoDB.",
   cvLink:
-    "https://drive.google.com/file/d/19iMGcZztoKCvO48zUmrwF0DoLCLM-6AB/view?usp=sharing",
+    "https://drive.google.com/file/d/1SC144RHiRs48sHEDdO2jEAOfqaM9rCfM/view?usp=sharing",
   img: "https://i.ibb.co/88pYr2X/1644657619001.jpg",
 };
 
